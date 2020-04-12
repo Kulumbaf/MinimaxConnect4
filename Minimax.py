fdc0c8d354ad26c55dc9169ea6d9243212287dea
Kulumbaf <francis.kulumba@edu.devinci.fr>
2020-04-13 01:42:39 +0200

    Adding minimax algorithm (sketch)

--- Minimax.py
+++ Minimax.py
@@ -1,11 +1,20 @@
 #12*6 connect 4
 import numpy as np
 from math import inf
+from collections import defaultdict
 
 class Minimax:
     def __init__(self):
-        self.position = 0
-        self.mask = 0
+        self.position = ''
+        self.mask = ''
+        self.human = ''
+        self.configure_dict()
+
+    def configure_dict(self):
+        self.moves = defaultdict(int)
+        self.moves['human'] = 0
+        self.moves['computer'] = 0
+        [self.moves[str(k)] for k in range(12)]
 
     def update_board(self, board, player):
         position, mask = '', ''
@@ -41,9 +50,49 @@ class Minimax:
         newPosition = position ^ mask #apply a XOR
         newMask = mask | (mask + (1 << col*7))
         return newPosition, newMask
+    
+    def undo_move(self, position, mask, col):
+        newMask = mask & (mask + (1 << (col*7)))
+        newPosition = position ^ mask
+        return newPosition, newMask
+    
+    def utility(self, computer=True):
+        return 22 - self.moves['computer'] if computer else -(22 - self.moves['human'])
 
-    def actions(self):
-        pass
+    def minimax(self, depth, alpha, beta, maxPlayer):
+        if depth==0 or self.terminal_test():
+            score = self.utility()
+            return [-1, -1, score]
+        
+        if maxPlayer:
+            maxScore = [-1, -1, -inf]
+            actions = self.actions()
+            for action in actions:
+                i, j = action[0], action[1]
+                self.state[i][j] = "O"
+                score = self.minimax(depth-1, alpha, beta, False)
+                self.state[i][j] = "."
+                score[0], score[1] = i, j
+                maxScore = maxScore if maxScore[2] >= score[2] else score
+                if maxScore[2] >= beta : 
+                    return maxScore
+                alpha = max(alpha, maxScore[2])
+            return maxScore
+        else :
+            minScore = [-1, -1, +inf]
+            actions = self.actions()
+            for action in actions:
+                i, j = action[0], action[1]
+                self.state[i][j] = "X"
+                score = self.minimax(depth-1, alpha, beta, True)
+                self.state[i][j] = "."
+                score[0], score[1] = i, j
+                minScore = minScore if minScore[2] <= score[2] else score
+                if minScore[2] <= alpha : 
+                    return minScore
+                beta = min(beta, minScore[2])
+            return minScore
 
 if __name__ == "__main__":
     ai = Minimax()
+    print(ai.moves)
