48c188e6bd5c496169e01ff8904634fe4bff1847
Kulumbaf <francis.kulumba@edu.devinci.fr>
2020-04-16 01:50:53 +0200

    Gamme implemented, solving a column index issue

--- Minimax.py
+++ Minimax.py
@@ -4,95 +4,124 @@ from math import inf
 from collections import defaultdict
 
 class Minimax:
-    def __init__(self):
-        self.position = ''
-        self.mask = ''
-        self.human = ''
+    def __init__(self, board, player):
+        self.update_board(board, player)
         self.configure_dict()
 
     def configure_dict(self):
         self.moves = defaultdict(int)
-        self.moves['human'] = 0
-        self.moves['computer'] = 0
+        self.playerCount = defaultdict(int)
+        self.playerCount['human'] = 0
+        self.playerCount['computer'] = 0
         [self.moves[str(k)] for k in range(12)]
 
     def update_board(self, board, player):
-        position, mask = '', ''
+        mask, position, human = '', '', ''
         for j in range(11, -1, -1):
             mask += '0'
             position += '0'
+            human += '0'
             #start with the upper row
             for i in range(6):
                 mask += ['0', '1'][board[i, j] != 0]
                 position += ['0', '1'][board[i, j] == player]
-        self.position, self.mask = int(position, 2), int(mask, 2)
+                human += ['0', '1'][board[i, j] == 1]
+        self.mask = int(mask, 2)
+        self.position = int(position, 2)
+        self.human = int(human, 2)
 
-    def connected_four(self, position):
+    def connected_four(self):
         #Horizontal check
-        check = position & (position >> 7)
+        check = self.position & (self.position >> 7)
         if check & (check >> 14) : return True
 
         #Diagonal \
-        check = position & (position)
+        check = self.position & (self.position)
         if check & (check >> 12) : return True
 
         #Diagonal /
-        check = position & (position >> 8)
+        check = self.position & (self.position >> 8)
         if check & (check >> 16) : return True
 
         #Vertical
-        check = position & (position >> 1)
+        check = self.position & (self.position >> 1)
+        if check & (check >> 2): return True
+        
+        #Human check
+        #Horizontal check
+        check = self.human & (self.human >> 7)
+        if check & (check >> 14) : return True
+
+        #Diagonal \
+        check = self.human & (self.human)
+        if check & (check >> 12) : return True
+
+        #Diagonal /
+        check = self.human & (self.human >> 8)
+        if check & (check >> 16) : return True
+
+        #Vertical
+        check = self.human & (self.human >> 1)
         if check & (check >> 2): return True
 
         return False
     
-    def make_move(self, position, mask, col):
-        newPosition = position ^ mask #apply a XOR
-        newMask = mask | (mask + (1 << col*7))
-        return newPosition, newMask
+    def make_move(self, col, maxPlayer):
+        if maxPlayer :
+            self.position = self.position ^ self.mask #apply a XOR
+            self.mask = self.mask | (self.mask + (1 << (col*7)))
+            #return newPosition, newMask
+        else :
+            self.human = self.human ^ self.mask #apply a XOR
+            self.mask = self.mask | (self.mask + (1 << (col*7)))
+            #return newPosition, newMask
     
-    def undo_move(self, position, mask, col):
-        newMask = mask & (mask + (1 << (col*7)))
-        newPosition = position ^ mask
-        return newPosition, newMask
+    def undo_move(self, col, maxPlayer):
+        if maxPlayer :
+            self.mask = self.mask & (self.mask + (1 << (col*7)))
+            self.position = self.position ^ self.mask
+            #return newPosition, self.self.mask
+        else :
+            self.mask = self.mask & (self.mask + (1 << (col*7)))
+            self.human = self.human ^ self.mask
+            #return newPosition, self.self.mask
+
+    def actions(self):
+        return [int(column) for column in self.moves if self.moves[column] <= 6]
     
     def utility(self, computer=True):
-        return 22 - self.moves['computer'] if computer else -(22 - self.moves['human'])
+        return -(37 - self.moves['human']) if computer else (37 - self.moves['computer'])
 
     def minimax(self, depth, alpha, beta, maxPlayer):
-        if depth==0 or self.terminal_test():
-            score = self.utility()
-            return [-1, -1, score]
+        if depth==0 or self.connected_four():
+            score = self.utility(maxPlayer)
+            return [-1, score]
         
         if maxPlayer:
-            maxScore = [-1, -1, -inf]
+            maxScore = [-1, -inf]
             actions = self.actions()
             for action in actions:
-                i, j = action[0], action[1]
-                self.state[i][j] = "O"
+                j = action
+                self.make_move(j, maxPlayer)
                 score = self.minimax(depth-1, alpha, beta, False)
-                self.state[i][j] = "."
-                score[0], score[1] = i, j
-                maxScore = maxScore if maxScore[2] >= score[2] else score
-                if maxScore[2] >= beta : 
+                self.undo_move(j, maxPlayer)
+                score[0] = j
+                maxScore = maxScore if maxScore[1] >= score[1] else score
+                if maxScore[1] >= beta : 
                     return maxScore
-                alpha = max(alpha, maxScore[2])
+                alpha = max(alpha, maxScore[1])
             return maxScore
         else :
-            minScore = [-1, -1, +inf]
+            minScore = [-1, +inf]
             actions = self.actions()
             for action in actions:
-                i, j = action[0], action[1]
-                self.state[i][j] = "X"
+                j = action
+                self.make_move(j, maxPlayer)
                 score = self.minimax(depth-1, alpha, beta, True)
-                self.state[i][j] = "."
-                score[0], score[1] = i, j
-                minScore = minScore if minScore[2] <= score[2] else score
-                if minScore[2] <= alpha : 
+                self.undo_move(j, maxPlayer)
+                score[0] = j
+                minScore = minScore if minScore[1] <= score[1] else score
+                if minScore[1] <= alpha : 
                     return minScore
-                beta = min(beta, minScore[2])
+                beta = min(beta, minScore[1])
             return minScore
-
-if __name__ == "__main__":
-    ai = Minimax()
-    print(ai.moves)
